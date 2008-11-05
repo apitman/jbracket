327848abe3689bffdb9422ceb723576c88204769
ken.a.hartsook <ken.a.hartsook@f2bff43c-95ac-11dd-b5ad-139240e4287b>
2008-11-05 23:16:14 +0000

    Test commit to make sure everything is working

--- src/org/vanderbilt/cs278/jbracket/model/Tournament.java
+++ src/org/vanderbilt/cs278/jbracket/model/Tournament.java
@@ -2,4 +2,8 @@ package org.vanderbilt.cs278.jbracket.model;
 
 public class Tournament {
 
+	public static void main (String[] args) {
+		System.out.println("Something compiles and runs");
+	}
+	
 }
