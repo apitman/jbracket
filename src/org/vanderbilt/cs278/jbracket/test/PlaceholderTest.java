77ee9d6d673dd7d5984bc17d73c007c3399055bb
ken.a.hartsook <ken.a.hartsook@f2bff43c-95ac-11dd-b5ad-139240e4287b>
2008-11-06 04:10:11 +0000

    Expanded project structure and added a rough draft of the ant script solution

---
+++ src/org/vanderbilt/cs278/jbracket/test/PlaceholderTest.java
@@ -0,0 +1,9 @@
+package org.vanderbilt.cs278.jbracket.test;
+
+import junit.framework.TestCase;
+
+public class PlaceholderTest extends TestCase {
+	public void testDummy() {
+		this.assertEquals(true,true);
+	}
+}
