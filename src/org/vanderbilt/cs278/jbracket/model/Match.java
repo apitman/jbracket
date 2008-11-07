ac9644761f1fbcea8d79c4d40979e621a49ea32c
ken.a.hartsook <ken.a.hartsook@f2bff43c-95ac-11dd-b5ad-139240e4287b>
2008-11-07 03:21:06 +0000

    Testing infrastructure commit

--- src/org/vanderbilt/cs278/jbracket/model/Match.java
+++ src/org/vanderbilt/cs278/jbracket/model/Match.java
@@ -1,3 +1,6 @@
+package org.vanderbilt.cs278.jbracket.model;
+
+import java.util.*;
 
 public interface Match {
 
---
+++ src/org/vanderbilt/cs278/jbracket/model/RankSeedingStrategy.java
@@ -0,0 +1,13 @@
+package org.vanderbilt.cs278.jbracket.model;
+
+import org.vanderbilt.cs278.jbracket.*;
+
+import java.util.List;
+
+public class RankSeedingStrategy implements SeedingStrategy {
+	
+	public void seed(List<Competitor> competitorList, Round round) {
+		
+	}
+
+}
